a31c683e486030865f4cb7e43014c13bf085805e
voditel546 <eroprapbyz1234@email.com>
2025-06-26 23:58:47 +0300

    Початковий код бота

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from mcstatus import JavaServer
 from telegram import Bot
 import asyncio
 from datetime import datetime
+from threading import Thread
+from flask import Flask
 
-# --- Налаштування ---
 BOT_TOKEN = '7968761400:AAGlkkcNI7XiSIWJK7DP6l9Dpk94jAKEF70'
 CHAT_ID = -1002424017400
 ADDRESS = "Na4narkamana.aternos.me"
@@ -11,8 +12,13 @@ PORT = 41833
 
 bot = Bot(token=BOT_TOKEN)
 server = JavaServer(ADDRESS, PORT)
-
 was_online = False
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Bot is running!"
 
 async def check_server():
     global was_online
@@ -27,9 +33,16 @@ async def check_server():
             await bot.send_message(chat_id=CHAT_ID, text=f"🔴 Час {now}\nСервер закрився!")
             was_online = False
 
-async def main():
+async def main_loop():
     while True:
         await check_server()
         await asyncio.sleep(60)
 
-asyncio.run(main())
+def run_flask():
+    app.run(host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    # Запускаємо Flask у окремому потоці
+    Thread(target=run_flask).start()
+    # Запускаємо головний цикл бота
+    asyncio.run(main_loop())
